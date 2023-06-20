0bde0fbb8ea7d972b85fbedade95b9fc0f81c73d
Denis Maslennikov <ktotom7@yandex.ru>
2023-06-20 23:44:54 +0300

    pep8 fix

--- exceptions.py
+++ exceptions.py
@@ -1,20 +1,17 @@
 class InvalidTokenException(Exception):
     """Вызывается если нет токена"""
-    def __int__(self, message='Некорректный токен'):
-        self.message=message
-        super(InvalidTokenDataException, self).__init__(self.message)
+    pass
 
 
 class InvalidChatIDException(Exception):
     """Вызывается если нет chat ID"""
-    def __int__(self, message='Некорректный chat ID'):
-        self.message = message
-        super(InvalidChatIDException, self).__int__(self.message)
+    pass
 
 
 class APIUnavailable(Exception):
     """Вызывается если статус ответа сервера API отличается от 200OK"""
     pass
+
 
 class WrongURL(Exception):
     """Вызывается если передано некорректное значение вместо URL"""
